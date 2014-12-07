1ec16335ef231f61dd062d3db4107d6bf328014c
Andy Baker <andy@andybak.net>
2014-12-07 14:10:23 +0000

    ooops. meant to be 2.7 not 1.7

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ setup(
     maintainer_email="hello+pleaseleavemealone@darylyu.com",
     url="http://www.xhtml2pdf.com",
     keywords="PDF, HTML, XHTML, XML, CSS",
-    install_requires = ["html5lib", "pyPdf2", "Pillow", "reportlab==1.7"],
+    install_requires = ["html5lib", "pyPdf2", "Pillow", "reportlab==2.7"],
     include_package_data=True,
     packages=find_packages(exclude=["tests", "tests.*"]),
     #    test_suite = "tests", They're not even working yet
